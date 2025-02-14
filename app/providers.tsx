93adcdff641c77a93fd35b79af13033a79b972a2
NyiHtutLwin05 <lwinnyihtut0@gmail.com>
2025-02-14 19:25:59 +0630

    default to light

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,13 @@
-'use client';
+"use client";
 
-import { ThemeProvider } from 'next-themes';
-import { I18nextProvider } from 'react-i18next';
-import i18n from '@/lib/i18n';
+import { ThemeProvider } from "next-themes";
+import { I18nextProvider } from "react-i18next";
+import i18n from "@/lib/i18n";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <I18nextProvider i18n={i18n}>
-        {children}
-      </I18nextProvider>
+    <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false}>
+      <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
     </ThemeProvider>
   );
 }
